6e9e74080a342fb594cb7502aa04d0e1f8096b6c
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-04-14 19:35:01 -0300

    Create ex003.py

---
+++ MUNDO 1 - Gustavo Guanabara/ex003.py
@@ -0,0 +1,10 @@
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+soma = n1 + n2
+print('A soma é', soma)
+
+# ou com ".format" diminuindo o código.
+
+num1 = int(input('Digite um número:'))
+num2 = int(input('Digite outro número:'))
+print('A soma é {}'.format(num1 + num2))
