62c14c7550b0d9437a7d72c379ce68b778eb8ab1
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-04-14 19:44:51 -0300

    Create ex007.py

---
+++ MUNDO 1 - Gustavo Guanabara/ex007.py
@@ -0,0 +1,10 @@
+n1 = float(input('Digite sua nota:'))
+n2 = float(input('Digite outra nota:'))
+md = (n1 + n2) /2
+print('A média é', md)
+
+#ou
+
+num1 = float(input('Digite uma nota:'))
+num2 = float(input('Digite outra nota:'))
+print('Á média é {}'.format((num1 + num2) / 2))
