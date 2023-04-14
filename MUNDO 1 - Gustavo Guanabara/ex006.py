ccca6ea3ff6c9d44297843a28831e7deb3990372
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-04-14 19:43:43 -0300

    Update ex006.py

--- MUNDO 1 - Gustavo Guanabara/ex006.py
+++ MUNDO 1 - Gustavo Guanabara/ex006.py
@@ -2,11 +2,11 @@ n =  int(input('Digite um número: '))
 d = n * 2
 t = n * 3
 r = n ** (1/2)
-print('Analisando o número digitado, o seu dobro é' , d)
+print('Analisando o número digitado, o seu dobro é ' , d)
 print('O seu triplo é ', t)
 print('É sua raiz é ', r)
 
-#ou usando \n é .format() para reduzir o codigo e criando uma nova linha a baixo na mesma linha
+#ou usando "\n" é ".format()" para reduzir o codigo e criando uma nova linha a baixo na mesma linha.
 
 num = int(input('Digite um número:'))
 print('Analisando o número digitado o seu dobro é {}. \nseu triplo {}. \ne sua raiz {:.2f}.'.format(num * 2, num * 3, (num **(1/2))))
