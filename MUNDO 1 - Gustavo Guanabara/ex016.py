ada41e75c035d6c494d02fe5a8dd7df17d0919bc
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-07-15 13:16:35 -0300

    Create ex016.py

---
+++ MUNDO 1 - Gustavo Guanabara/ex016.py
@@ -0,0 +1,27 @@
+'''
+    #importação de toda funcionalidade especifico da biblioteca
+
+import math           
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))
+
+
+
+
+
+
+    #importação de um metodo especifico da biblioteca
+
+from math import trunc              
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, trunc(num)))'''
+
+
+
+
+
+    
+     #sem importa modulos
+
+num = float(input('Digite um valor: '))
+print('O valor digitado foi {} e a sua porção inteira é {}'.format(num, int(num)))
