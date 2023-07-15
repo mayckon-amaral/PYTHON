b5cc52bdd2aa8ed43bb67c2a8f1e21e1c249b092
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-07-15 13:51:13 -0300

    Create ex017.py

---
+++ MUNDO 1 - Gustavo Guanabara/ex017.py
@@ -0,0 +1,32 @@
+    #sem importa modulos
+
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = (co ** 2 + ca ** 2) ** (1/2)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+
+
+
+
+
+ '''   #importação de toda funcionalidade especifico da biblioteca
+
+import math
+
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))
+
+
+
+
+    #importação de um metodo especifico da biblioteca
+
+from math import hypot
+
+co = float(input('Comprimento do cateto oposto: '))
+ca = float(input('Comprimento do cateto adjacente: '))
+hi = hypot(co, ca)
+print('A hipotenusa vai medir {:.2f}'.format(hi))'''
