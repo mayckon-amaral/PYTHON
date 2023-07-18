a686e44c458518c42c975367a364a86922fe14c3
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-07-18 17:45:57 -0300

    Create ex021.py

---
+++ MUNDO 1 - Gustavo Guanabara/ex021.py
@@ -0,0 +1,13 @@
+#biblioteca pygame    version python 3.6+
+import pygame  
+pygame.mixer.init()
+pygame.init()
+pygame.mixer.music.load('test.mp3')
+pygame.mixer_music.play()
+pygame.event.wait()
+
+
+
+# biblioteca playsound     version python 3.6
+from playsound import playsound
+playsound('test.mp3')
