6bff420232b2b4a2466029ef80e7e218a785df67
Mayckon Amaral <89323911+mayckon-amaral@users.noreply.github.com>
2023-07-30 12:08:25 -0300

    Update ex005.py

--- MUNDO 1 - Gustavo Guanabara/ex005.py
+++ MUNDO 1 - Gustavo Guanabara/ex005.py
@@ -9,3 +9,9 @@ num = int(input('Digite um número: '))
 sc = num + 1
 at = num - 1
 print('Analisando o número digitado seu sucessor é {} é seu antecessor é {}'.format(num + 1, num - 1))
+
+
+# Redução de código
+
+num = int(input('Digite um número: '))
+print('Analisando o número digitado seu sucessor é {} é seu antecessor é {}'.format(num+1, num-1))
